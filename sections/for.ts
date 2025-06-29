ffb24abebfd2f252db880b461984ff3290b36af5
alexvasquez199914 <alexvasquez.leon1999@gmail.com>
2025-06-29 16:27:35 -0500

    feature/agregando-ciclo-for

---
+++ sections/for.ts
@@ -0,0 +1,17 @@
+console.log('Hola Mundo actualizado3');
+
+let limit = 100;
+
+for (let i = 1 ; i <= limit; i++) {
+
+    if (i === 5){
+        continue;
+    }
+
+    console.log('let i:',i);
+
+    if (i === 5 ){
+        break;
+    }
+}
+console.log('Fin del programa')
